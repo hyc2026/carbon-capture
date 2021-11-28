f56f58bfa6a15ba38705ed73cc6cbc10999e06b2
v-bingwang <v-bingwang@microsoft.com>
2021-11-28 15:00:41 +0800

    planning policy 去掉多余import，合并工具函数到policy文件，方便提交

--- algorithms/planning_policy/planning_policy.py
+++ algorithms/planning_policy/planning_policy.py
@@ -1,24 +1,95 @@
 from os import cpu_count
 import sys
-
+import numpy as np
 from numpy import positive
-
-from utils.parallel_env import worker
 
 sys.path.append('.')
 sys.path.append('..')
 sys.path.append('../..')
 import copy
-from abc import ABC, ABCMeta, abstractmethod
-from typing import Dict, OrderedDict, Tuple
+from abc import ABC, abstractmethod
+from typing import Dict, Tuple
 from math import log, exp
-from algorithms.base_policy import BasePolicy
-from envs.obs_parser_xinnian import (BaseActions, ObservationParser,
-                                     WorkerActions, WorkerDirections)
+
 from zerosum_env.envs.carbon.helpers import (Board, Cell, Collector, Planter,
                                              Point, RecrtCenter, Worker,
                                              RecrtCenterAction, WorkerAction)
 from random import randint, shuffle
+
+
+BaseActions = [None,
+               RecrtCenterAction.RECCOLLECTOR,
+               RecrtCenterAction.RECPLANTER]
+
+WorkerActions = [None,
+                 WorkerAction.UP,
+                 WorkerAction.RIGHT,
+                 WorkerAction.DOWN,
+                 WorkerAction.LEFT]
+
+WorkerDirections = np.stack([np.array((0, 0)),
+                             np.array((0, 1)),
+                             np.array((1, 0)),
+                             np.array((0, -1)),
+                             np.array((-1, 0))])  # 与WorkerActions相对应
+
+
+class BasePolicy:
+    """
+    Base policy class that wraps actor and critic models to calculate actions and value for training and evaluating.
+    """
+    def __init__(self):
+        pass
+
+    def policy_reset(self, episode: int, n_episodes: int):
+        """
+        Policy Reset at the beginning of the new episode.
+        :param episode: (int) current episode
+        :param n_episodes: (int) number of total episodes
+        """
+        pass
+
+    def can_sample_trajectory(self) -> bool:
+        """
+        Specifies whether the policy's actions output and values output can be collected for training or not
+            (default False).
+        :return: True means the policy's trajectory data can be collected for training, otherwise False.
+        """
+        return False
+
+    def get_actions(self, observation, available_actions=None) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Compute actions predictions for the given inputs.
+        :param observation:  (np.ndarray) local agent inputs to the actor.
+        :param available_actions: (np.ndarray) denotes which actions are available to agent
+                                  (if None, all actions available)
+
+        :return actions: (np.ndarray) actions to take.
+        :return action_log_probs: (np.ndarray) log probabilities of chosen actions.
+        """
+        raise NotImplementedError("not implemented")
+
+
+    @staticmethod
+    def to_env_commands(policy_actions: Dict[str, int]) -> Dict[str, str]:
+        """
+        Actions output from policy convert to actions environment can accept.
+        :param policy_actions: (Dict[str, int]) Policy actions which specifies the agent name and action value.
+        :return env_commands: (Dict[str, str]) Commands environment can accept,
+            which specifies the agent name and the command string (None is the stay command, no need to send!).
+        """
+
+        def agent_action(agent_name, command_value) -> str:
+            # hack here, 判断是否为转化中心,然后返回各自的命令
+            actions = BaseActions if 'recrtCenter' in agent_name else WorkerActions
+            return actions[command_value].name if 0 < command_value < len(actions) else None
+
+        env_commands = {}
+        for agent_id, cmd_value in policy_actions.items():
+            command = agent_action(agent_id, cmd_value)
+            if command is not None:
+                env_commands[agent_id] = command
+        return env_commands
 
 
 # Plan是一个Agent行动的目标，它可以由一个Action完成(比如招募捕碳者），也可以由多
@@ -315,7 +386,7 @@ class PlanterGoToAndPlantTreeAtTreeAtPlan(PlanterPlan):
             #                 'cell_distance_weight']
             total_carbon = self.get_total_carbon()
             self.preference_index = total_carbon * 0.9625 ** distance
-            print(self.preference_index)
+            # print(self.preference_index)
 
     def check_validity(self):
         #没有开启
