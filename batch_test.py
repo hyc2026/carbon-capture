b15c9b69d1f166307758c584ae0d5a5b0891ac51
youralmight <youralmight@gmail.com>
2021-11-28 16:08:15 +0800

    修正了batch_test.py中的bug

--- batch_test.py
+++ batch_test.py
@@ -13,10 +13,9 @@ from algorithms.jiaqi_policy.my_policy import MyPolicy
 policy_class_list = [PlanningPolicy, MyPolicy]
 expriment_repeat = 5
 
-def run_expiriment(policy_class_A,policy_class_B):
+def run_experiment(policy_class_A,policy_class_B):
     policy_A = policy_class_A()
     policy_B = policy_class_B()
-
 
     logs=[]
     env = make(
@@ -34,32 +33,45 @@ def run_expiriment(policy_class_A,policy_class_B):
     player_a, player_b = info[-1]
     if player_a['reward'] == player_b['reward']:
         print("平局!")
-        result.dual += 1
+        result.dual = 1
     elif player_a['reward'] is None:
         print(f"队伍B获胜 ({player_b['reward']})!")
-        result.B_win_A_error += 1
+        result.B_win_A_error = 1
     elif player_b['reward'] is None:
         print(f"队伍A获胜 ({player_a['reward']})!")
-        result.A_win_B_error += 1
+        result.A_win_B_error = 1
     elif player_a['reward'] > player_b['reward']:
         print(f"队伍A获胜 ({player_a['reward']})!")
-        result.A_normal_win += 1
+        result.A_normal_win = 1
     else:
         print(f"队伍B获胜 ({player_b['reward']})!")
-        result.B_normal_win += 1
-        
+        result.B_normal_win = 1
+    
+    result.A_relative_win=result.A_win_B_error+result.A_normal_win -result.B_win_A_error-result.B_normal_win
+    result.experiment_count=1
+    
     print(f"{policy_A.__class__} VS {policy_B.__class__}")
     print(result)
     
-    
+
+def run_experiments(policy_class_A,policy_class_B,experiment_count:int):
+    total_result=None
+    for i in range(experiment_count):
+        single_experiment_result=run_experiment(policy_class_A,policy_class_B)
+        if total_result is None:
+            total_result=single_experiment_result
+        else:
+            for key in total_result.keys():
+                total_result[key]=single_experiment_result[key]
+    return total_result
 
 def main():
     random.seed(0)
     for policy_class_A in policy_class_list:
         for policy_class_B in policy_class_list:
-            if policy_class_A is policy_class_B: break
+            if policy_class_A is policy_class_B: continue
             for i in range(expriment_repeat):
-                run_expiriment(policy_class_A,policy_class_B)
+                run_experiment(policy_class_A,policy_class_B)
             
 
 if __name__ == "__main__":
